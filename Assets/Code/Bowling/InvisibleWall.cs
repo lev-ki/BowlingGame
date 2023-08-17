5a810a3cb1aa81d1ede84086f02c5491ca4d101e
Alex <alexsmekhunov@gmail.com>
2023-08-17 21:48:15 +0300

    Added invisible walls

---
+++ Assets/Code/Bowling/InvisibleWall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Bowling
+{
+    public class InvisibleWall : MonoBehaviour
+    {
+        [SerializeField] LayerMask bottleLayerMask;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if ((1 << other.gameObject.layer & bottleLayerMask) != 0)
+            {
+                GameManager.Instance.InvokeEvent(EventId.BottleFell);
+            }
+        }
+    }
+}
