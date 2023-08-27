55bd64b3c130fbf97874ff97d1f0f0ef482a08b6
leosergkie <leosergkie@gmail.com>
2023-08-27 22:42:22 +0200

    More fixes

--- Assets/Code/Progression/Round.cs
+++ Assets/Code/Progression/Round.cs
@@ -12,7 +12,7 @@ namespace Code.Progression
         public List<BallLaunch> launches;
 
         public bool resetPins;
-        [Obsolete] public bool resetBottle;
+        public bool resetBottle;
 
         public float spillThreshold = 20;
         public float spillSpeed = 1;
